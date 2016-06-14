d8816fa0e8db3543b79e5b8704f086300ebe3b88
Rui Liu <liurui1217@gmail.com>
2016-06-14 10:30:58 -0400

    Fast Matrix-vector multiplication function

---
+++ src/FastMatrixVectorMultiplication.hpp
@@ -0,0 +1,18 @@
+
+/// \brief Fast Matrix-Vector Multiplication;
+/// \param y = Ax, on host memory
+/// \param A matrix, on host memory
+/// \param x vector to be multiplied, on host memory
+/// \param m row number of the matrix
+/// \param n col number of the matrix
+void FMVM(float* y, float* A, float* x, int m, int n);
+/// \brief Fast Matrix-Vector Multiplication;
+/// \param d_y = Ax, on device memory
+/// \param d_A matrix, on device memory
+/// \param d_x vector to be multiplied, on device memory
+/// \param m row number of the matrix
+/// \param n col number of the matrix
+void FMVM_device(float* d_y, float* dA, float* d_x, int m, int n);
+
+/// \brief Test the Fast Matrix Vector Multiplication function
+void testFMVM();
