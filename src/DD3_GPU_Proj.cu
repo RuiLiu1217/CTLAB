f798d4ec31f8d5f78050604e14a32c1956140dbe
Rui Liu <liurui1217@gmail.com>
2021-11-17 01:56:36 -0500

    Add TextureObjectProvider which is separated from DD3_GPU_Proj.cu

--- src/DD3_GPU_Proj.cu
+++ src/DD3_GPU_Proj.cu
@@ -3,60 +3,11 @@
 #include "DD3_GPU_Proj.h"
 #include "CTLAB.h"
 #include "Geometry.h"
+#include "TextureObjectProvider.h"
 
 #define BLKX 32
 #define BLKY 8
 #define BLKZ 1
-
-//Create texture object and corresponding cudaArray function
-template<typename T>
-void createTextureObject(
-	cudaTextureObject_t& texObj, //return: texture object pointing to the cudaArray
-	cudaArray* d_prjArray, // return: cudaArray storing the data
-	int Width, int Height, int Depth, // data size
-	T* sourceData, // where is the data
-	cudaMemcpyKind memcpyKind, // data from host or memory
-	cudaTextureAddressMode addressMode, // how to address the texture (clamp, border ...)
-	cudaTextureFilterMode textureFilterMode, // usually linear filtering (double --> int2 use pointer not linear interpolation)
-	cudaTextureReadMode textureReadMode, // usually use element wise reading mode.
-	bool isNormalized) // usually false
-{
-	cudaExtent prjSize;
-	prjSize.width = Width;
-	prjSize.height = Height;
-	prjSize.depth = Depth;
-	cudaChannelFormatDesc channelDesc = cudaCreateChannelDesc<T>();
-	cudaMalloc3DArray(&d_prjArray, &channelDesc, prjSize);
-	cudaMemcpy3DParms copyParams = { 0 };
-	copyParams.srcPtr = make_cudaPitchedPtr(
-		(void*)sourceData, prjSize.width * sizeof(T),
-		prjSize.width, prjSize.height);
-	copyParams.dstArray = d_prjArray;
-	copyParams.extent = prjSize;
-	copyParams.kind = memcpyKind;
-	cudaMemcpy3D(&copyParams);
-	cudaResourceDesc resDesc;
-	memset(&resDesc, 0, sizeof(resDesc));
-	resDesc.resType = cudaResourceTypeArray;
-	resDesc.res.array.array = d_prjArray;
-	cudaTextureDesc texDesc;
-	memset(&texDesc, 0, sizeof(texDesc));
-	texDesc.addressMode[0] = addressMode;
-	texDesc.addressMode[1] = addressMode;
-	texDesc.addressMode[2] = addressMode;
-	texDesc.filterMode = textureFilterMode;
-	texDesc.readMode = textureReadMode;
-
-	texDesc.normalizedCoords = isNormalized;
-	CUDA_SAFE_CALL(cudaCreateTextureObject(&texObj, &resDesc, &texDesc, nullptr));
-}
-
-// Destroy a GPU array and corresponding TextureObject
-void destroyTextureObject(cudaTextureObject_t& texObj, cudaArray* d_array)
-{
-	cudaDestroyTextureObject(texObj);
-	cudaFreeArray(d_array);
-}
 
 template<typename Ta, typename Tb>
 __global__ void naive_copyToTwoVolumes(Ta* in_ZXY,
@@ -2569,20 +2520,6 @@ void DD3Proj_gpu(
 		break;
 	}
 }
-
-//class Proj
-//{
-//
-//};
-//
-//class Proj_BranchLess : Proj
-//{
-//private:
-//	static __global__ void hello(){}
-//
-//};
-
-
 
 //Use the split-collect method to do the projection
 extern "C"
---
+++ src/TextureObjectProvider.h
@@ -0,0 +1,55 @@
+#ifndef TEXTURE_OBJECT_PROVIDER_H_
+#define TEXTURE_OBJECT_PROVIDER_H_
+#include <cuda_runtime.h>
+#include <cuda.h>
+//Create texture object and corresponding cudaArray function
+template<typename T>
+void createTextureObject(
+	cudaTextureObject_t& texObj, //return: texture object pointing to the cudaArray
+	cudaArray* d_prjArray, // return: cudaArray storing the data
+	int Width, int Height, int Depth, // data size
+	T* sourceData, // where is the data
+	cudaMemcpyKind memcpyKind, // data from host or memory
+	cudaTextureAddressMode addressMode, // how to address the texture (clamp, border ...)
+	cudaTextureFilterMode textureFilterMode, // usually linear filtering (double --> int2 use pointer not linear interpolation)
+	cudaTextureReadMode textureReadMode, // usually use element wise reading mode.
+	bool isNormalized) // usually false
+{
+	cudaExtent prjSize;
+	prjSize.width = Width;
+	prjSize.height = Height;
+	prjSize.depth = Depth;
+	cudaChannelFormatDesc channelDesc = cudaCreateChannelDesc<T>();
+	cudaMalloc3DArray(&d_prjArray, &channelDesc, prjSize);
+	cudaMemcpy3DParms copyParams = { 0 };
+	copyParams.srcPtr = make_cudaPitchedPtr(
+		(void*)sourceData, prjSize.width * sizeof(T),
+		prjSize.width, prjSize.height);
+	copyParams.dstArray = d_prjArray;
+	copyParams.extent = prjSize;
+	copyParams.kind = memcpyKind;
+	cudaMemcpy3D(&copyParams);
+	cudaResourceDesc resDesc;
+	memset(&resDesc, 0, sizeof(resDesc));
+	resDesc.resType = cudaResourceTypeArray;
+	resDesc.res.array.array = d_prjArray;
+	cudaTextureDesc texDesc;
+	memset(&texDesc, 0, sizeof(texDesc));
+	texDesc.addressMode[0] = addressMode;
+	texDesc.addressMode[1] = addressMode;
+	texDesc.addressMode[2] = addressMode;
+	texDesc.filterMode = textureFilterMode;
+	texDesc.readMode = textureReadMode;
+
+	texDesc.normalizedCoords = isNormalized;
+	CUDA_SAFE_CALL(cudaCreateTextureObject(&texObj, &resDesc, &texDesc, nullptr));
+}
+
+// Destroy a GPU array and corresponding TextureObject
+void destroyTextureObject(cudaTextureObject_t& texObj, cudaArray* d_array)
+{
+	cudaDestroyTextureObject(texObj);
+	cudaFreeArray(d_array);
+}
+
+#endif
